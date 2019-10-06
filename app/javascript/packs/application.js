73308e11a82ec65b4735034ccee348569c765630
Steven Wagner <steven@sales-ignition.com>
2019-10-06 09:37:56 -0400

    java script issues with rails 6.0 fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery-rails")
+require("jquery3.4.1")
 require("popper")
 require("bootstrap-sprockets")
 
